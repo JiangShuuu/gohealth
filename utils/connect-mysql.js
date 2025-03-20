df62074947e18478e88eaa67b82ad32925b6ffa2
Jiangshuuu <globelex65@gmail.com>
2025-03-20 18:25:42 +0800

    upd

--- utils/connect-mysql.js
+++ utils/connect-mysql.js
@@ -1,16 +1,15 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME
-} = process.env;
+import "dotenv/config.js";
 
-console.log({ DB_HOST, DB_USER, DB_PASS, DB_NAME
-});
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env;
 
 const db = await mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
+  port: DB_PORT,
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0,
